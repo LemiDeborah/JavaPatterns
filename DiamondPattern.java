020b7cd0eacaed7b52bebe2b5441a3bfa69447f2
LemiDeborah <133324750+LemiDeborah@users.noreply.github.com>
2024-07-23 22:31:25 +0530

    Create DiamondPattern.java

---
+++ DiamondPattern.java
@@ -0,0 +1,36 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++){
+                    System.out.print(" *");
+                }
+                
+                
+            System.out.println();
+        }
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=i;j<=n;j++){
+                    System.out.print(" *");
+                }
+            System.out.println();
+        }
+    }
+}
+OUTPUT:
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *
+   * * * *
+    * * *
+     * *
+      *
