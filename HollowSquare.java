80bc9b85f7cad492aaef3ce7a9b9dd49160b9532
LemiDeborah <133324750+LemiDeborah@users.noreply.github.com>
2024-07-23 22:14:12 +0530

    Create HollowSquare.java

---
+++ HollowSquare.java
@@ -0,0 +1,23 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+OUTPUT:
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
